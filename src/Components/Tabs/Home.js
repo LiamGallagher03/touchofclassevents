1461da7eb5a9d633c4dda70b695f6cf1aee6b17e
saraiyakrs <718618@sylvaniastudents.org>
2022-05-06 08:27:28 -0500

    convention

--- src/Components/Tabs/Home.js
+++ src/Components/Tabs/Home.js
@@ -1,10 +1,10 @@
 function Home() {
     return (
         <div>
-            <h1>Welcome to Sylvania International Community Center</h1>
-            <img src="communitycenter.jpg" alt="Community Center"></img>
+            <h1>Welcome to Sylvania International Convention Center</h1>
+            <img src="communitycenter.jpg" alt="Convention Center"></img>
             <figcaption>Our Center</figcaption>
-            <p>At the Sylvania International Community Center, you can register any event! Explore our website to find numerous possibilities!</p>
+            <p>At the Sylvania International Convention Center, you can register any event! Explore our website to find numerous possibilities!</p>
             <p><strong>Login/Register</strong> allows you to login to a pre-existing account or to register for an account. You must have an account to register an event.</p>
             <p><strong>Contact Us</strong> Any questions? Please contact us!</p>
             <p><strong>About us</strong> Find out more about our history that is totally NOT made up!</p>
